1f5591bd383d01ee8e1a0a70383b237a4d613bce
edzhabs <edzhabs@gmail.com>
2024-07-01 20:41:30 +0800

    Build Issue Summary

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import prisma from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
 import LatestIssue from "./LatestIssue";
 
-export default function Home() {
-  return <LatestIssue />;
+export default async function Home() {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const in_progress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+  return <IssueSummary open={open} in_progress={in_progress} closed={closed} />;
 }
